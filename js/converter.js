37d46ff644984a564b13ae561fb2139a272442d4
William Eduardo Guevara Alfaro <ia.williameduardo@ufg.edu.sv>
2024-08-29 07:52:33 -0600

    finalizacion de codigo HTML, finalizacion de hojqa de estilos, inicio de logica del negocio

---
+++ js/converter.js
@@ -0,0 +1,7 @@
+const inputfield= document.getElementById("input-temp");
+const fromUnitField= document.getElementById("input-temp");
+const toUnitField= document.getElementById("output-unit");
+const outputField = document.getElementById("output-temp");
+const form= document.getElementById("converter");
+
+console.log("William Eduardo Guevara Alfaro")
